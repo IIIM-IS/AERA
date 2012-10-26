f331284fc07bd416b8318db9f2686b5234984706
Kristinn Thorisson <thorisson@ru.is>
2012-10-26 15:48:51 +0000

    * New license header added
    git-svn-id: http://cadia.ru.is/svn/repos/replicode@407 36b273a1-5dce-4f9d-8980-b90fca7d7570

--- usr_operators/Callbacks/callbacks.h
+++ usr_operators/Callbacks/callbacks.h
@@ -1,32 +1,79 @@
-//	callbacks.h
-//
-//	Author: Eric Nivel
-//
-//	BSD license:
-//	Copyright (c) 2010, Eric Nivel
-//	All rights reserved.
-//	Redistribution and use in source and binary forms, with or without
-//	modification, are permitted provided that the following conditions are met:
-//
-//   - Redistributions of source code must retain the above copyright
-//     notice, this list of conditions and the following disclaimer.
-//   - Redistributions in binary form must reproduce the above copyright
-//     notice, this list of conditions and the following disclaimer in the
-//     documentation and/or other materials provided with the distribution.
-//   - Neither the name of Eric Nivel nor the
-//     names of their contributors may be used to endorse or promote products
-//     derived from this software without specific prior written permission.
-//
-//	THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
-//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-//	DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
-//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//_/_/
+//_/_/ HUMANOBS - Replicode Callbacks
+//_/_/
+//_/_/ Eric Nivel
+//_/_/ Center for Analysis and Design of Intelligent Agents
+//_/_/   Reykjavik University, Menntavegur 1, 101 Reykjavik, Iceland
+//_/_/   http://cadia.ru.is
+//_/_/ Copyright(c)2012
+//_/_/
+//_/_/ This software was developed by the above copyright holder as part of 
+//_/_/ the HUMANOBS EU research project, in collaboration with the 
+//_/_/ following parties:
+//_/_/ 
+//_/_/ Autonomous Systems Laboratory
+//_/_/   Technical University of Madrid, Spain
+//_/_/   http://www.aslab.org/
+//_/_/
+//_/_/ Communicative Machines
+//_/_/   Edinburgh, United Kingdom
+//_/_/   http://www.cmlabs.com/
+//_/_/
+//_/_/ Istituto Dalle Molle di Studi sull'Intelligenza Artificiale
+//_/_/   University of Lugano and SUPSI, Switzerland
+//_/_/   http://www.idsia.ch/
+//_/_/
+//_/_/ Institute of Cognitive Sciences and Technologies
+//_/_/   Consiglio Nazionale delle Ricerche, Italy
+//_/_/   http://www.istc.cnr.it/
+//_/_/
+//_/_/ Dipartimento di Ingegneria Informatica
+//_/_/   University of Palermo, Italy
+//_/_/   http://roboticslab.dinfo.unipa.it/index.php/Main/HomePage
+//_/_/
+//_/_/
+//_/_/ --- HUMANOBS Open-Source BSD License, with CADIA Clause v 1.0 ---
+//_/_/
+//_/_/ Redistribution and use in source and binary forms, with or without 
+//_/_/ modification, is permitted provided that the following conditions 
+//_/_/ are met:
+//_/_/
+//_/_/ - Redistributions of source code must retain the above copyright 
+//_/_/ and collaboration notice, this list of conditions and the 
+//_/_/ following disclaimer.
+//_/_/
+//_/_/ - Redistributions in binary form must reproduce the above copyright 
+//_/_/ notice, this list of conditions and the following
+//_/_/ disclaimer in the documentation and/or other materials provided 
+//_/_/ with the distribution.
+//_/_/
+//_/_/ - Neither the name of its copyright holders nor the names of its 
+//_/_/ contributors may be used to endorse or promote products 
+//_/_/ derived from this software without specific prior written permission.
+//_/_/
+//_/_/ - CADIA Clause: The license granted in and to the software under this 
+//_/_/ agreement is a limited-use license. The software may not be used in 
+//_/_/ furtherance of: 
+//_/_/ (i) intentionally causing bodily injury or severe emotional distress 
+//_/_/ to any person; 
+//_/_/ (ii) invading the personal privacy or violating the human rights of 
+//_/_/ any person; or 
+//_/_/ (iii) committing or preparing for any act of war.
+//_/_/
+//_/_/ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//_/_/ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//_/_/ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+//_/_/ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+//_/_/ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//_/_/ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//_/_/ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+//_/_/ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+//_/_/ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+//_/_/ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//_/_/ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//_/_/
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
 #ifndef	callbacks_h
 #define	callbacks_h
