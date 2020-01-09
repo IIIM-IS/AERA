bfca4e83777537d5bec606814c1d7a967bfc2a71
Jeff Thompson <jeff@thefirst.org>
2020-01-09 13:38:06 +0100

    TestMem: When babbling, inject the move goal into primary.

--- Test/test_mem.cpp
+++ Test/test_mem.cpp
@@ -338,7 +338,7 @@ template<class O, class S> void TestMem<O, S>::onTimeTick() {
         (discretePositionObj_, position_property_, discretePosition_,
           now, now + sampling_period_us);
 
-      const uint64 babbleStopTime_us = 2600000;
+      const uint64 babbleStopTime_us = 2900000;
       if (now - Utils::GetTimeReference() < babbleStopTime_us) {
         // Babble.
         if (discretePosition_ == y0Ent_)
@@ -372,7 +372,7 @@ template<class O, class S> void TestMem<O, S>::onTimeTick() {
         r_exec::Fact* factCmd = new r_exec::Fact
           (cmd, now + sampling_period_us, now + 2*sampling_period_us, 1, 1);
         r_exec::Goal* goal = new r_exec::Goal(factCmd, get_self(), 1);
-        injectFact(goal, now + sampling_period_us, now + sampling_period_us, get_stdin());
+        injectFact(goal, now + sampling_period_us, now + sampling_period_us, primary_group_);
       }
     }
   }
