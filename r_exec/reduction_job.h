57afc9f45f1c176da75eb137c9a130b439c83e63
Jeff Thompson <jeff@thefirst.org>
2020-05-10 17:21:50 +0200

    ReductionJob: ifdef WITH_DEBUG_OID then log the reduction job.

--- r_exec/reduction_job.h
+++ r_exec/reduction_job.h
@@ -107,6 +107,12 @@ public:
   bool update(Timestamp now) {
 
     _Mem::Get()->register_reduction_job_latency(now - ijt_);
+#ifdef WITH_DEBUG_OID
+    cout << Utils::RelativeTime(now) << " ReductionJob " << get_job_id() << 
+      "(" << get_debug_oid() << "): controller(" << processor_->get_debug_oid() <<
+      ")->reduce(View(fact_" << input_->object_->get_oid() << "(" <<
+      input_->object_->get_debug_oid() << ")))" << endl;
+#endif
     processor_->reduce(input_);
     return true;
   }
@@ -126,6 +132,11 @@ public:
   bool update(Timestamp now) {
 
     _Mem::Get()->register_reduction_job_latency(now - ijt_);
+#ifdef WITH_DEBUG_OID
+    cout << Utils::RelativeTime(now) << " BatchReductionJob " << get_job_id() <<
+      "(" << get_debug_oid() << "): controller(" << controller_->get_debug_oid() << 
+      "), trigger fact(" << trigger_->get_debug_oid() << ")" << endl;
+#endif
     processor_->reduce_batch(trigger_, controller_);
     return true;
   }
