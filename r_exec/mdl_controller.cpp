151c31625a1ae938873c812fa2a9c9aca737a6a7
jefft0 <jeff@thefirst.org>
2021-09-30 15:15:11 +0200

    PrimaryMDLController: In store_requirement, also signal r_monitors for all simulated requirements, including negative. See pull request #182.

--- r_exec/mdl_controller.cpp
+++ r_exec/mdl_controller.cpp
@@ -1529,8 +1529,10 @@ void PrimaryMDLController::store_requirement(_Fact *f_p_f_imdl, MDLController *c
       _store_requirement(&simulated_requirements_.negative_evidences_, e);
   }
 
-  // In case of a positive requirement, tell monitors they can check for chaining again.
-  if (f_imdl->is_fact()) {
+  // In case of a positive non-simulated requirement or any simulated requirement (positive or negative),
+  // tell monitors they can check for chaining again. Even a simulated negative (strong) requirement may cause
+  // check_simulated_imdl to take some action (such as to invalidate a defeasible prediction).
+  if (f_imdl->is_fact() || is_simulation) {
     r_code::list<P<_GMonitor> >::const_iterator m;
     g_monitorsCS_.enter();
     for (m = r_monitors_.begin(); m != r_monitors_.end();) { // signal r-monitors.
