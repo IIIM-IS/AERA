e15c07f8d30518f99027a866ee91e9a86a15adae
jefft0 <jeff@thefirst.org>
2023-07-07 07:47:25 +0200

    _TPX: Split out build_mdl_head_from_abstract from build_mdl_head. (Same functionality.)

--- r_exec/pattern_extractor.h
+++ r_exec/pattern_extractor.h
@@ -254,6 +254,7 @@ protected:
   r_code::Code *build_cst(const std::vector<Component> &components, BindingMap *bm, _Fact *main_component);
 
   r_code::Code *build_mdl_head(HLPBindingMap *bm, uint16 tpl_arg_count, _Fact *lhs, _Fact *rhs, uint16 &write_index, bool allow_shared_timing_vars = true);
+  r_code::Code* build_mdl_head_from_abstract(uint16 tpl_arg_count, r_code::Code* lhs, r_code::Code* rhs, uint16& write_index);
   void build_mdl_tail(r_code::Code *mdl, uint16 write_index);
 
   void inject_hlps() const;
