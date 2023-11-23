456aa80c4186280fc3e34dc658eb8a844f4d2893
jefft0 <jeff@thefirst.org>
2023-11-23 09:08:07 +0100

    PrimaryMDLController: In predict, make the MkRdx with ground_mk_rdx instead of ground.

--- r_exec/mdl_controller.cpp
+++ r_exec/mdl_controller.cpp
@@ -1678,7 +1678,7 @@ void PrimaryMDLController::predict(HLPBindingMap *bm, _Fact *input, Fact *f_imdl
 
         Code *mk_rdx;
         if (ground)
-          mk_rdx = new MkRdx(f_imdl, (Code *)input, ground, production, 1, bm);
+          mk_rdx = new MkRdx(f_imdl, (Code *)input, ground_mk_rdx, production, 1, bm);
         else
           mk_rdx = new MkRdx(f_imdl, (Code *)input, production, 1, bm);
         bool rate_failures = inject_prediction(production, f_imdl, confidence, before - now, mk_rdx);
