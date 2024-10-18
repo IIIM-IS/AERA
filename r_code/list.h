b4ca8265b8b6a84a75e7a8508392e3b6cce095d0
jefft0 <jeff@thefirst.org>
2024-10-18 19:03:25 +0200

    r_code: In list iterators, disambiguate the _iterator base class. (Don't know why gcc wants this.)

--- r_code/list.h
+++ r_code/list.h
@@ -270,23 +270,23 @@ public:
     const_iterator(const list *l, int32 c) : _iterator(c), list_(l) {}
   public:
     const_iterator() : _iterator(), list_(NULL) {}
-    const T &operator *() const { return list_->cells_[cell_].data_; }
-    const T *operator ->() const { return &(list_->cells_[cell_].data_); }
+    const T &operator *() const { return list_->cells_[_iterator::cell_].data_; }
+    const T *operator ->() const { return &(list_->cells_[_iterator::cell_].data_); }
     const_iterator &operator ++() {
 
-      cell_ = list_->cells_[cell_].next_;
+      _iterator::cell_ = list_->cells_[_iterator::cell_].next_;
       return *this;
     }
     const_iterator &operator =(const const_iterator &i) {
 
       list_ = i.list_;
-      cell_ = i.cell_;
+      _iterator::cell_ = i.cell_;
       return *this;
     }
     const_iterator &operator =(const iterator &i) {
 
       list_ = i.list_;
-      cell_ = i.cell_;
+      _iterator::cell_ = i.cell_;
       return *this;
     }
   };
@@ -300,17 +300,17 @@ public:
     iterator(list *l, int32 c) : _iterator(c), list_(l) {}
   public:
     iterator() : _iterator(), list_(NULL) {}
-    T &operator *() const { return list_->cells_[cell_].data_; }
-    T *operator ->() const { return &(list_->cells_[cell_].data_); }
+    T &operator *() const { return list_->cells_[_iterator::cell_].data_; }
+    T *operator ->() const { return &(list_->cells_[_iterator::cell_].data_); }
     iterator &operator ++() {
 
-      cell_ = list_->cells_[cell_].next_;
+      _iterator::cell_ = list_->cells_[_iterator::cell_].next_;
       return *this;
     }
     iterator &operator =(const iterator &i) {
 
       list_ = i.list_;
-      cell_ = i.cell_;
+      _iterator::cell_ = i.cell_;
       return *this;
     }
   };
