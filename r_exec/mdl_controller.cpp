7019e84efbc1e599b511bbfb42a1267dd20a465c
jefft0 <jeff@thefirst.org>
2026-07-16 13:40:02 +0200

    test: Revert  PrimaryMDLController::reduce to the original. Fix model number in hgs-GTPX.txt

--- r_exec/mdl_controller.cpp
+++ r_exec/mdl_controller.cpp
@@ -1796,8 +1796,7 @@ void PrimaryMDLController::reduce(r_exec::View *input) { // no lock.
     MatchResult match_result = bm->match_bwd_lenient(goal_target, rhs_);
     switch (match_result) {
     case MATCH_SUCCESS_NEGATIVE:
-        // Don't allow negative match.
-        break;
+      opposite = true;
     case MATCH_SUCCESS_POSITIVE:
       abduce(bm, (Fact *)input->object_, opposite, confidence);
       break;
