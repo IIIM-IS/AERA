759eb13d2cbafdfaae97c4a90080118d24442acf
Fedcmm <federico.cruciani@live.com>
2026-07-27 15:22:37 +0200

    Add travel to place example

--- AERA/main.cpp
+++ AERA/main.cpp
@@ -88,6 +88,7 @@
 #include "IODevices/TCP/tcp_io_device.h"
 #include "IODevices/video_screen/video_screen_io_device.h"
 #include "tower_of_hanoi_io_device.h"
+#include "take_vehicles_io_device.h"
 #include "../usr_operators/usr_operators.h"
 #include "test_mem.h"
 #include "../r_exec/init.h"
@@ -363,6 +364,9 @@ int32 start_AERA(const char* file_name, const char* decompiled_file_name) {
     }
     else if (settings.io_device_.compare("tower_of_hanoi") == 0) {
       mem = new tower_of_hanoi::TowerOfHanoiIODevice(settings.source_file_name_);
+    }
+    else if (settings.io_device_.compare("take_vehicles") == 0) {
+      mem = new take_vehicles::TakeVehiclesIODevice();
     }
     else {
       std::cout << "Unrecognized io_device \"" << settings.io_device_ << "\"" << std::endl;
